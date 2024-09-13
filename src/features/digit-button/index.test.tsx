c9c809612fdd685dacf4c56ea7637bf67f9534bb
kuman514 <hadjadj0@gmail.com>
2024-09-13 21:34:30 +0900

    test: add required tests for number-button

---
+++ src/features/digit-button/index.test.tsx
@@ -0,0 +1,14 @@
+import { cleanup } from '@testing-library/react';
+import { beforeEach, describe, it } from 'vitest';
+
+describe('DigitButton', () => {
+  beforeEach(() => {
+    cleanup();
+  });
+
+  it('should be colored when it is active', () => {});
+
+  it('should be clear when it is inactive', () => {});
+
+  it('should be call on-click function with its digit as a parameter', () => {});
+});
