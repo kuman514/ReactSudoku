ffcf62f5c79268570e9c32eb0f3216aacd61e38b
kuman514 <hadjadj0@gmail.com>
2024-09-14 03:04:31 +0900

    feat: add digit-button & and its grid area

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,56 @@
-import { Tile } from '^/shared/tile';
+import { DigitButton } from '^/features/digit-button';
+import { Digit } from '^/features/digit-button/types';
+import { useState } from 'react';
 
 export function App() {
+  const [digitActiveStatus, setDigitActiveStatus] = useState<
+    Record<Digit, boolean>
+  >({
+    0: false,
+    1: false,
+    2: false,
+    3: false,
+    4: false,
+    5: false,
+    6: false,
+    7: false,
+    8: false,
+    9: false,
+  });
+
+  function handleOnClickDigitButton(clickedDigit: Digit) {
+    setDigitActiveStatus({
+      ...digitActiveStatus,
+      [clickedDigit]: !digitActiveStatus[clickedDigit],
+    });
+  }
+
   return (
     <main>
-      <Tile isColored>1</Tile>
-      <Tile isColored>2</Tile>
-      <Tile isColored>3</Tile>
-      <Tile>4</Tile>
-      <Tile>5</Tile>
-      <Tile>6</Tile>
-      <Tile isColored>7</Tile>
-      <Tile isColored>8</Tile>
-      <Tile isColored>9</Tile>
+      <div
+        style={{
+          width: 300,
+          display: 'grid',
+          gridTemplateAreas: `
+            "a b c"
+            "d e f"
+            "g h i"
+            ". j ."
+          `,
+          alignItems: 'center',
+        }}
+      >
+        {([1, 2, 3, 4, 5, 6, 7, 8, 9, 0] satisfies Digit[]).map(
+          (digit: Digit) => (
+            <DigitButton
+              key={`digit-button-${digit}`}
+              digit={digit}
+              isActive={digitActiveStatus[digit]}
+              onClick={handleOnClickDigitButton}
+            />
+          )
+        )}
+      </div>
     </main>
   );
 }
