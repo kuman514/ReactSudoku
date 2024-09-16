190ac19507006ba504d746770e36dd8f6c7aa85b
kuman514 <hadjadj0@gmail.com>
2024-09-16 10:17:26 +0900

    test: add required tests for mode-button

---
+++ src/features/mode-button/index.test.tsx
@@ -0,0 +1,16 @@
+import { cleanup } from '@testing-library/react';
+import { beforeEach, describe, it } from 'vitest';
+
+describe('ModeButton', () => {
+  beforeEach(() => {
+    cleanup();
+  });
+
+  it('should be colored when it is active', () => {});
+
+  it('should be clear when it is inactive', () => {});
+
+  it('should call on-click function', async () => {});
+
+  it('should not call on-click function when it is disabled', async () => {});
+});
